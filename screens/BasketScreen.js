ed8f601e19e1d378b203c826e51e0160dac742c6
fekri el farhani <99203411+elfarhaniFekri@users.noreply.github.com>
2022-10-26 17:32:33 +0100

    Update BasketScreen.js

--- screens/BasketScreen.js
+++ screens/BasketScreen.js
@@ -58,7 +58,7 @@ const BasketScreen = () => {
         <View className='flex-row items-center space-x-4 px-4 py-3 bg-white my-5'>
           <Image
             source={{
-              uri: 'https://links.papareact.com/wru',
+              uri: 'https://images.prismic.io/dbhq-deliveroo-riders-website/ed825791-0ba4-452c-b2cb-b5381067aad3_RW_hk_kit_importance.png?auto=compress,format&rect=0,0,1753,1816&w=1400&h=1450',
             }}
             className='h-7 w-7 bg-gray-300 p-4 rounded-full'
           />
